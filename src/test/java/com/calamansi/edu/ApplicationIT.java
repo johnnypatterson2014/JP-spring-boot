427a38a826554404c61eda567e07ad9cd1764efa
johnnypatterson2014 <johnny.patterson@gmail.com>
2025-04-28 14:54:07 -0700

    latest working code and tests

--- src/test/java/com/calamansi/edu/ApplicationIT.java
+++ src/test/java/com/calamansi/edu/ApplicationIT.java
@@ -1,28 +1,10 @@
 package com.calamansi.edu;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK, classes = Application.class)
-@AutoConfigureMockMvc
-@TestPropertySource(locations = "classpath:application-integrationtest.properties")
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK, classes = Application.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ApplicationIT {
 
-	@Autowired
-	private MockMvc mvc;
-
-	@Test
-	public void givenX_whenGetX_thenStatus200() throws Exception {
-
-		mvc.perform(get("/api/test"))
-			.andExpectAll(status().isOk(), jsonPath("$.test").value("testing"));
-	}
+	
 }
